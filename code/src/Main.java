6fe772ab08303363296f302bd75b6e8147e47836
Vlad is Law <nandate@yandex.ru>
2023-03-28 02:11:59 +0300

    v0.2

--- code/src/Main.java
+++ code/src/Main.java
@@ -1,48 +1,51 @@
-import java.util.Arrays;
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scannerInput = new Scanner(System.in);
 
-        System.out.print("Введите арифметическое выражение: ");
-        String output = calc(String.valueOf(scannerInput.nextLine()));
+        System.out.print("Введите арифметическое выражение: "); // optional
+        String output = calc(scannerInput.nextLine().replaceAll(" ", ""));
 
-        System.out.print("Результат работы калькулятора: ");
+        System.out.print("Результат работы калькулятора: "); // optional
         System.out.println(output);
     }
 
-    public static String calc(String input){
+    public static @NotNull String calc(String input){
 
         Operations expression = new Operations();
 
         char sign = expression.signCheck(input);
-        System.out.println("Оператор: " + sign);
+        System.out.println("Оператор: " + sign); // debug
+
+        boolean roman = expression.romanCheck(input);
+        System.out.println("Римские цифры : " + roman); // debug
 
         int[] numbers = expression.valueCheck(input);
-//        int[] numbers = {5, 10};
-        System.out.println("Числа: " + numbers[0] + ", " + numbers[1]);
+        System.out.println("Числа: " + numbers[0] + ", " + numbers[1]); // debug
 
-        int output = 0;
+
+        int result;
         switch (sign) {
-            case '+' -> output = (numbers[0] + numbers[1]);
-            case '-' -> output = (numbers[0] - numbers[1]);
-            case '*' -> output = (numbers[0] * numbers[1]);
-            case '/' -> output = (numbers[0] / numbers[1]);
-            default -> throw new RuntimeException("Арифметическая операция не распознана: " + sign);
+            case '+' -> result = (numbers[0] + numbers[1]);
+            case '-' -> result = (numbers[0] - numbers[1]);
+            case '*' -> result = (numbers[0] * numbers[1]);
+            case '/' -> result = (numbers[0] / numbers[1]);
+            default -> throw new RuntimeException("Арифметическая операция не распознана");
         }
-        return String.valueOf(output);
+        return String.valueOf(result);
     }
 
     static class Operations {
 
-        char signCheck(String input){
-
+        char signCheck(@NotNull String input){
             char sign;
-            if (contains(input, "+")) {
-                sign = '+';
-            } else if (contains(input, "-")) {
+            if (contains(input, "-")) {
                 sign = '-';
+            } else if (contains(input, "+")) {
+                sign = '+';
             } else if (contains(input, "*")) {
                 sign = '*';
             } else if (contains(input, "/")) {
@@ -53,16 +56,30 @@ public class Main {
             return sign;
         }
 
-        boolean romanCheck(String input) {
-            
-            return true;
+        boolean romanCheck(@NotNull String input) {
+            String[] stringValues = input.split("\\" + signCheck(input), 2);
+            boolean check1;
+            boolean check2;
+
+            switch (stringValues[0]){
+                case "I", "II", "III", "IV", "V", "VI", "VII", "VIII","IX", "X" -> check1 = true;
+                default -> check1 = false;
+            }
+            switch (stringValues[1]){
+                case "I", "II", "III", "IV", "V", "VI", "VII", "VIII","IX", "X" -> check2 = true;
+                default -> check2 = false;
+            }
+            if (check1 && check2) {
+                return true;
+            } else if (check1 == check2){
+                return false;
+            } else {
+                throw new RuntimeException("Неверный формат арифметического выражения");
+            }
         }
 
-        int[] valueCheck(String input) {
-
-            String[] stringValues = input.split("\\" + signCheck(input));
-
-//            System.out.println(Arrays.toString(numbers));
+        int[] valueCheck(@NotNull String input) {
+            String[] stringValues = input.split("\\" + signCheck(input), 2);
 
             try {
                 return new int[]{Integer.parseInt(stringValues[0]), Integer.parseInt(stringValues[1])};
@@ -73,7 +90,7 @@ public class Main {
 
     }
 
-    static boolean contains(String content, String lookingFor) {
+    static boolean contains(@NotNull String content, String lookingFor) {
         return content.contains(lookingFor);
     }
 
