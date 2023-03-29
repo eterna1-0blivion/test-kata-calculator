08c8924bbc73c174b2db8c0672ded6fb347b5f39
Vlad is Law <nandate@yandex.ru>
2023-03-29 18:06:57 +0300

    v1.2
    Fixed problem with Roman numerals.
    General code refactoring.

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Java Calculator
+
 * Performs the simplest operations with Arabic `(1, 4, 9)` and Roman `(I, IV, IX)` numerals.
-* Accepts simple operators `+, -, *, /` and numbers from `1` to `10`. _Fractional remainder is discarded._
-* Output of Roman numerals is also limited from `I` to `X`.
+* Accepts simple operators `(+, -, *, /)` and numbers from `1` to `10`.
+* Fractional remainder is discarded.
+* Output value of Roman numerals cannot be less than `I (1)`.
+
 ###
+
 **_Test Assignment for Kata Academy as a Java Programmer._**
--- code/src/Main.java
+++ code/src/Main.java
@@ -1,23 +1,30 @@
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
     static boolean optional = true; // это для user-friendly интерфейса, но можно выключить
-    static boolean debug = false; // это для дебага, можно включить
+    static boolean debug = false; // это для дебага, можно включить. Ниже настройки:
+    static boolean signDebug = true; // показывает найденную арифметическую операцию (только при debug = true)
+    static boolean valueDebug = true; // показывает найденные введённые значения (только при debug = true)
+    static boolean romanDebug = true; // показывает, найдены ли римские символы во введённом выражении (только при debug = true)
 
     public static void main(String[] args) {
 
         Scanner scannerInput = new Scanner(System.in);
 
-        if(optional) System.out.print("Введите арифметическое выражение: ");
-        String output = calc(scannerInput.nextLine().replaceAll(" ", ""));
+        if (optional) System.out.print("Введите арифметическое выражение: ");
+        String output = calc(scannerInput.nextLine().replaceAll(" ", "").toUpperCase());
 
-        if(optional) System.out.print("Результат работы калькулятора: ");
+        if (optional) System.out.print("Результат работы калькулятора: ");
         System.out.println(output);
     }
 
-    public static @NotNull String calc(String input){
+    public static String calc(String input) {
 
         Operations expression = new Operations();
 
@@ -34,22 +41,43 @@ public class Main {
             case '/' -> resultArabic = (values[0] / values[1]);
             default -> throw new RuntimeException("Арифметическая операция не распознана");
         }
-        if (roman){
-            resultRoman = expression.reverseConversion(resultArabic);
+        if (roman) {
+            resultRoman = expression.arabicToRoman(resultArabic);
             return resultRoman;
         } else {
             return String.valueOf(resultArabic);
         }
     }
 
-//    Это костыль, не смотри сюда. И не трогай. Кыш-кыш
-    static boolean signDebug = true;
-    static boolean valueDebug = true;
-    static boolean romanDebug = true;
+    static boolean contains(@NotNull String content, String lookingFor) { // метод для поиска арифметического знака
+        return content.contains(lookingFor);
+    }
+
+    enum RomanNumeral { // 'расшифровка' риских чисел в арабские
+        I(1), IV(4), V(5), IX(9), X(10),
+        XL(40), L(50), XC(90), C(100),
+        CD(400), D(500), CM(900), M(1000);
+
+        final int value;
+
+        RomanNumeral(int value) {
+            this.value = value;
+        }
+
+        static List<RomanNumeral> getReverseSortedValues() {
+            return Arrays.stream(values())
+                    .sorted(Comparator.comparing((RomanNumeral e) -> e.value).reversed())
+                    .collect(Collectors.toList());
+        }
+
+        int getValue() {
+            return value;
+        }
+    }
 
     static class Operations {
 
-        char signCheck(@NotNull String input){ // проверка арифметической операции (знака)
+        char signCheck(String input) { // проверка арифметической операции (знака)
             char sign;
             if (contains(input, "-")) {
                 sign = '-';
@@ -62,100 +90,118 @@ public class Main {
             } else {
                 throw new RuntimeException("Арифметическая операция не распознана");
             }
-            if(debug && signDebug) {System.out.println("Арифметический оператор: " + sign); signDebug = false;}
+            if (debug && signDebug) {
+                System.out.println("Арифметический оператор: " + sign);
+                signDebug = false;
+            }
             return sign;
         }
 
-        boolean romanCheck(@NotNull String input) { // првоерка на наличие римских цифр
+        boolean romanCheck(@NotNull String input) { // првоерка на наличие римских цифр (и ограничение ввода от 1 до 10)
             String[] stringValues = input.split("\\" + signCheck(input), 2);
             boolean check1;
             boolean check2;
 
-            switch (stringValues[0]){
-                case "I", "II", "III", "IV", "V", "VI", "VII", "VIII","IX", "X" -> check1 = true;
+            switch (stringValues[0]) {
+                case "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X" -> check1 = true;
                 default -> check1 = false;
             }
-            switch (stringValues[1]){
-                case "I", "II", "III", "IV", "V", "VI", "VII", "VIII","IX", "X" -> check2 = true;
+            switch (stringValues[1]) {
+                case "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X" -> check2 = true;
                 default -> check2 = false;
             }
 
             if (check1 && check2) { // + проверка смешивания римских и арабских цифр
-                if(debug && romanDebug) System.out.println("Римские цифры: Да"); romanDebug = false;
+                if (debug && romanDebug) System.out.println("Римские цифры: Да");
+                romanDebug = false;
                 return true;
-            } else if (check1 == check2){
-                if(debug && romanDebug) System.out.println("Римские цифры: Нет"); romanDebug = false;
+            } else if (check1 == check2) {
+                if (debug && romanDebug) System.out.println("Римские цифры: Нет");
+                romanDebug = false;
                 return false;
             } else {
                 throw new RuntimeException("Неверный формат написания римских цифр");
             }
         }
 
-        int[] conversion(@NotNull String input) { // преобразование из римских в арабские с помощью Enum (он внизу)
+        int[] romanToArabic(@NotNull String input) { // преобразование из римских в арабские с помощью Enum (он внизу)
             String[] stringValues = input.split("\\" + signCheck(input), 2);
-            try {
-                if (romanCheck(input)) {
-                    return new int[]{RomanNumerals.valueOf(stringValues[0]).getArabic(), RomanNumerals.valueOf(stringValues[1]).getArabic()};
-                } else {
-                    return new int[]{Integer.parseInt(stringValues[0]), Integer.parseInt(stringValues[1])};
+            int[] intValues = {0, 0};
+            int i = 0;
+
+            List<RomanNumeral> romanNumerals = RomanNumeral.getReverseSortedValues();
+
+            if (romanCheck(input)) {
+                for (int k = 0; k < stringValues.length; k++) {
+
+                    while ((stringValues[k].length() > 0) && (i < romanNumerals.size())) {
+                        RomanNumeral symbol = romanNumerals.get(i);
+                        if (stringValues[k].startsWith(symbol.name())) {
+                            intValues[k] += symbol.getValue();
+                            stringValues[k] = stringValues[k].substring(symbol.name().length());
+                        } else {
+                            i++;
+                        }
+                    }
+                    i = 0;
+
+                    if (stringValues[k].length() > 0) {
+                        throw new RuntimeException("Неверный формат арифметического выражения");
+                    }
                 }
-            } catch (NumberFormatException e) {
-                throw new RuntimeException("Неверный формат арифметического выражения");
-            }
-
-        }
-
-        String reverseConversion(int input) { // преобразование из арабских в римские (Enum не принимает в себя int, к сожалению)
-            String result;
-            switch (input) {
-                case 1 -> result = "I";
-                case 2 -> result = "II";
-                case 3 -> result = "III";
-                case 4 -> result = "IV";
-                case 5 -> result = "V";
-                case 6 -> result = "VI";
-                case 7 -> result = "VII";
-                case 8 -> result = "VIII";
-                case 9 -> result = "IX";
-                case 10 -> result = "X";
-                default -> throw new RuntimeException("Выходное значение римских цифр не может быть больше 10 или меньше 1");
-            }
-            return result;
-        }
-
-        int[] valueCheck(@NotNull String input) { // проверка входных значений
-            int[] intValues = conversion(input);
-
-            if (intValues[0] > 10 || intValues[0] < 1) {
-                throw new RuntimeException("Входное значение не может быть больше 10 или меньше 1");
-            } else if (intValues[1] > 10 || intValues[1] < 1) {
-                throw new RuntimeException("Входное значение не может быть больше 10 или меньше 1");
+                return intValues;
             } else {
                 try {
-                    if(debug && valueDebug) { System.out.println("Обнаруженные операнды: " + intValues[0] + " и " + intValues[1]); valueDebug = false;}
-                    return new int[]{intValues[0],intValues[1]};
+                    return new int[]{Integer.parseInt(stringValues[0]), Integer.parseInt(stringValues[1])};
                 } catch (NumberFormatException e) {
                     throw new RuntimeException("Неверный формат арифметического выражения");
                 }
             }
+
         }
 
-    }
+        String arabicToRoman(int input) { // преобразование из арабских в римские с помощью Enum (он внизу)
 
-    static boolean contains(@NotNull String content, String lookingFor) { // метод для поиска арифметического знака (и не только)
-        return content.contains(lookingFor);
-    }
+            if (input < 1 || input > 3999) {
+                throw new RuntimeException("Римские числа не могут принимать значения меньше 1 (I) и больше 3999 (MMMCMXCIX)");
+            }
 
-    enum RomanNumerals { // 'расшифровка' риских чисел в арабские
-        I(1), II(2), III(3), IV(4), V(5), VI(6), VII(7), VIII(8), IX(9), X(10);
+            List<RomanNumeral> romanNumerals = RomanNumeral.getReverseSortedValues();
+            int i = 0;
+            StringBuilder sb = new StringBuilder();
 
-        final int arabic;
-        RomanNumerals(int arabic) {
-            this.arabic = arabic;
+            while (input > 0 && i < romanNumerals.size()) {
+                RomanNumeral currentSymbol = romanNumerals.get(i);
+                if (currentSymbol.getValue() <= input) {
+                    sb.append(currentSymbol.name());
+                    input -= currentSymbol.getValue();
+                } else {
+                    i++;
+                }
+            }
+
+            return sb.toString();
         }
-        int getArabic() {
-            return arabic;
+
+        int[] valueCheck(String input) { // проверка входных значений
+            int[] intValues = romanToArabic(input);
+
+            if ((intValues[0] < 1 || intValues[0] > 10) || (intValues[1] < 1 || intValues[1] > 10)) {
+                throw new RuntimeException("Входное значение не может быть меньше 1 или больше 10");
+            } else {
+                try {
+                    if (debug && valueDebug) {
+                        System.out.println("Обнаруженные операнды: " + intValues[0] + " и " + intValues[1]);
+                        valueDebug = false;
+                    }
+                    return new int[]{intValues[0], intValues[1]};
+                } catch (NumberFormatException e) {
+                    throw new RuntimeException("Неверный формат арифметического выражения");
+                }
+            }
+
         }
+
     }
 
 }
